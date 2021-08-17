397a0029fd9d5e13f0eaeffd35b66f808127b87b
Lucas Souza <lucas.rsouza2000@gmail.com>
2021-08-16 23:05:26 -0300

    implementado controller da agenda
    Melhorado rotinas de manipulação da agenda, onde foi incluso status de retorno

--- src/controller/agendaController.js
+++ src/controller/agendaController.js
@@ -3,42 +3,76 @@ const Agenda = require('../model/Agenda');
 module.exports = {
 
     async show(req, res) {
-        const { ddata, dhora, nsala } = req.body;
+        try {
+            const data = req.param.data;
+            const hora = req.param.hora;
+            const sala = req.param.sala;
 
-        let agenda = await Agenda.findOne( ddata, dhora, nsala);
-
-        return res.json(agenda);
+            let agenda = await Agenda.where({ ddata: data, dhora: hora, nsala: sala });
+            if (agenda)
+                return res.status(200).json(agenda);
+            else
+                return res.status(404).send({ error : 'Not Found'});
+        } catch (e) {
+            res.status(500).send(e.message);
+        }
     },
 
     async index(req, res) {
-        let agenda = await Agenda.find();
+        try {
+            let agenda = await Agenda.find(req.param.id);
 
-        return res.json(agenda);
+            if (agenda)
+                return res.status(201).json(agenda);
+            else
+                return res.status(404).send({ error : 'Not Found'});
+        } catch (e) {
+            res.status(500).send(e.message);
+        }
     },
 
     async store(req, res) {
-        const { ddata, dhora, nsala } = req.body;
-        const data = new Date();
+        try {
+            const { ddata, dhora, nsala } = req.body;
+            const data = new Date();
 
-        let agenda = await Agenda.findOne({ ddata, dhora, nsala });
+            let agenda = await Agenda.findOne({ ddata, dhora, nsala });
 
-        if (!agenda)
-            retorno = await Agenda.create({ ddata, dhora, nsala });
-        else
-            return res.status(400).send({ error : 'Not authorized'}) //retorno = { "mensagem" : "Horario já reservado"};
-            
-        return res.json(retorno);
+            if (!agenda) {
+                retorno = await Agenda.create({ ddata, dhora, nsala });
+                return res.status(201).json(retorno);
+            } else
+                return res.status(400).send({ error : 'Not authorized'});
+
+        } catch (e) {
+            res.status(500).send(e.message);
+        }
     },
     
     async update(req, res) {
-        
+        try {
+            const { ddata, dhora, nsala } = req.body;
+            let agenda = await Agenda.findOne({ ddata, dhora, nsala });
+
+            if (agenda) {
+                const updateAgenda = await agenda.update(_id, { ddata, dhora, nsala });
+                return res.status(200).json(retorno);
+            } else
+                return res.status(400).send({ error : 'Not authorized'});
+        } catch (e) {
+            res.status(500).send(e.message);
+        }
     },
 
     async destroy(req, res) {
-        const { ddata, dhora, nsala } = req.body;
+        try {
+            const { ddata, dhora, nsala } = req.body;
 
-        const agenda = await Agenda.findOneAndDelete({ ddata, dhora, nsala });
+            const agenda = await Agenda.findOneAndDelete({ ddata, dhora, nsala });
 
-        await agenda.destroy();
+            await agenda.destroy();
+        } catch (e) {
+            res.status(500).send(e.message);
+        }
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,8 @@ const agendaController = require('../src/controller/AgendaController');
 
 routes.post('/agenda/reserva', agendaController.store);
 routes.get('/agenda/listagem', agendaController.index);
+routes.get('/agenda/consulta', agendaController.show);
+routes.put('/agenda/alterar', agendaController.update);
+routes.post('/agenda/deletar', agendaController.destroy);
 
 module.exports = routes;
